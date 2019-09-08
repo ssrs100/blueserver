a2640891e81a6f332ec5f5bcb2e5951cad87ad77
ssrs100 <18126523585@163.com>
2019-09-08 15:37:30 +0800

    api middleware

--- cmd/blueserver.go
+++ cmd/blueserver.go
@@ -8,7 +8,6 @@ import (
 	"github.com/jack0liu/utils"
 	"github.com/ssrs100/blueserver/bluedb"
 	"github.com/ssrs100/blueserver/controller"
-	"github.com/ssrs100/blueserver/controller/aws"
 	"github.com/ssrs100/blueserver/influxdb"
 	"github.com/ssrs100/blueserver/mqttclient"
 	"net/http"
@@ -27,69 +26,9 @@ type Server struct {
 
 var s Server
 
-func Health(w http.ResponseWriter, req *http.Request, _ map[string]string) {
-	w.WriteHeader(http.StatusOK)
-}
-
 func (s *Server) RegisterRoutes() *httptreemux.TreeMux {
 	logs.Debug("Setting route info...")
-
-	// Set the router.
-	router := httptreemux.New()
-
-	// Set router options.
-	router.PanicHandler = httptreemux.SimplePanicHandler
-	router.RedirectTrailingSlash = true
-
-	// Set the routes for the application.
-
-	// Route for health check
-	router.GET("/v1/heart", Health)
-
-	// Routes for users
-	router.GET("/v1/users", controller.GetUsers)
-	router.GET("/v1/users/:projectId", controller.GetUser)
-	router.POST("/v1/users", controller.CreateUser)
-	router.POST("/v1/users/login", controller.UserLogin)
-	router.DELETE("/v1/users/:projectId", controller.DeleteUser)
-	router.POST("/v1/users/:projectId", controller.BindAwsUser)
-
-	// Routes for beacons
-	router.POST("/proximity/v1/:projectId/beacons", controller.RegisterBeacon)
-	router.GET("/proximity/v1/:projectId/beacons", controller.ListBeacons)
-	router.DELETE("/proximity/v1/:projectId/beacons/:beaconId", controller.DeleteBeacon)
-	router.PUT("/proximity/v1/:projectId/beacons/:beaconId", controller.UpdateBeacon)
-	router.POST("/proximity/v1/:projectId/beacons/:beaconId/active", controller.ActiveBeancon)
-	router.POST("/proximity/v1/:projectId/beacons/:beaconId/deactive", controller.DeActiveBeancon)
-
-	// Routes for attachments
-	router.POST("/proximity/v1/:projectId/beacons/:beaconId/attachments", controller.CreateAttachment)
-	router.DELETE("/proximity/v1/:projectId/beacons/:beaconId/attachments/:attachmentId", controller.DeleteAttachment)
-	router.DELETE("/proximity/v1/:projectId/beacons/:beaconId/attachments", controller.DeleteAttachmentByBeacon)
-	router.GET("/proximity/v1/:projectId/beacons/:beaconId/attachments", controller.GetAttachmentByBeacon)
-
-	// Routes for hybrid
-	router.POST("/proximity/v1/:projectId/getforobserved", controller.GetForObserved)
-
-	// Routes for components
-	router.POST("/equipment/v1/:projectId/components", controller.RegisterComponent)
-	router.GET("/equipment/v1/:projectId/components", controller.ListComponents)
-	router.DELETE("/equipment/v1/:projectId/components/:componentId", controller.DeleteComponent)
-	router.PUT("/equipment/v1/:projectId/components/:componentId", controller.UpdateComponent)
-	router.GET("/equipment/v1/:projectId/components/:componentId/collections", controller.ListCollections)
-
-	// Routes for component detail
-	router.GET("/equipment/v1/:projectId/components/:componentId/detail", controller.GetComponentDetail)
-	router.PUT("/equipment/v1/:projectId/components/:componentId/detail", controller.UpdateComponentDetail)
-	router.PUT("/equipment/v1/:projectId/components/:componentId/detail/cancel-modifying", controller.CancelUpdateDetail)
-	router.PUT("/equipment/v1/:projectId/components/:componentId/detail/sync", controller.SyncComponentDetail)
-
-	router.GET("/aws/v1/:projectId/things", aws.ListThings)
-	router.GET("/aws/v1/:projectId/things/:thingName/latest", aws.GetThingLatestData)
-	router.GET("/aws/v1/:projectId/things/:thingName/range-data", aws.GetThingData)
-	// Routes for attachments
-	router.GET("/app/resource", controller.GetAdPic)
-	return router
+	return controller.LoadApi()
 }
 
 var stop = make(chan os.Signal)
