c67956cd00068c8734ef846902543c125d7b6e74
ssrs100 <18126523585@163.com>
2019-09-13 15:01:24 +0800

    query devices

--- awsmqtt/aws_client.go
+++ awsmqtt/aws_client.go
@@ -120,6 +120,7 @@ func (ac *AwsIotClient) startAwsClient(projectId string, stop chan interface{}) 
 				}
 				rd.Thing = s.Thing
 				rd.ProjectId = projectId
+				logs.Info("%v", rd)
 				if err := influxdb.Insert("temperature", &rd); err != nil {
 					logs.Error("%s", err.Error())
 					continue
